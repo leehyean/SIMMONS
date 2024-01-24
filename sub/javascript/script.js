d734972b54f6a7061d8249433690c3ff19d55687
leehyean <93965591+leehyean@users.noreply.github.com>
2024-01-24 13:52:40 +0900

    서브 페이지(ABOUT US)
    서브 페이지(ABOUT US) - 회사 소개 페이지 반응형 제작

---
+++ sub/javascript/script.js
@@ -0,0 +1,30 @@
+
+AOS.init();
+
+// var swiper = new Swiper(".section_1 .mySwiper", {
+//   direction: 'horizontal',	
+//   spaceBetween: 0,
+//   pagination: {
+//     el: ".swiper-pagination",
+//     clickable: true,
+//   }, 
+//   navigation: {
+//     nextEl: ".swiper-button-next",
+//     prevEl: ".swiper-button-prev",
+//   },
+//   loop: true,
+//     autoplay: true,
+//     autoplay: {
+//       delay: 4000,
+  
+//     },
+//   observer: true,
+//   observerParents: true,
+  
+// });
+
+
+// section_1
+
+
+
